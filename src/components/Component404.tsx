7ddeed2d86df039b4007290fe58823f7c2af683d
KSJ710 <KSJ710@example.com>
2022-02-16 23:21:39 +0900

    404をコンポーネント化

---
+++ src/components/Component404.tsx
@@ -0,0 +1,6 @@
+import { useState } from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
+
+export default function Sam(): JSX.Element {
+  return <div>404</div>;
+}
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,7 @@
+import Component404 from 'src/components/Component404';
+
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>;
+  return <Component404 />;
 }
 
 Custom404.props = {
