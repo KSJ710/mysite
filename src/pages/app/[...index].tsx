717a4d4c3ef82bada4e804f064d023f638793923
KSJ710 <KSJ710@example.com>
2022-02-16 23:21:24 +0900

    条件分岐の記述を変更

--- src/pages/app/[...index].tsx
+++ src/pages/app/[...index].tsx
@@ -1,17 +1,18 @@
 import { useRouter } from 'next/router';
 import Home from 'src/components/app/Home';
 import MemberNew from 'src/components/app/member/New';
+import Component404 from 'src/components/Component404';
 
 export default function App(): JSX.Element {
   const router = useRouter();
   const { index } = router.query;
-
-  return (
-    <>
-      {index?.[0] === 'home' ? <Home /> : null}
-      {index?.[0] === 'member' && index?.[1] === 'new' ? <MemberNew /> : null}
-    </>
-  );
+  if (index?.[0] === 'home') {
+    return <Home />;
+  } else if (index?.[0] === 'member' && index?.[1] === 'new') {
+    return <MemberNew />;
+  } else {
+    return <Component404 />;
+  }
 }
 
 App.props = {
