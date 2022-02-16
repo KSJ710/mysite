e07dee773db0652576f1176f7156cbe17c827919
KSJ710 <KSJ710@example.com>
2022-02-17 01:29:59 +0900

    パス修正

--- src/pages/api/member/[resources].ts
+++ src/pages/api/member/[resources].ts
@@ -15,7 +15,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     case 'POST':
       const saltRounds = 10;
       bcrypt.hash(req.body.password, saltRounds, async function (err, hash) {
-        if (err) res.redirect(303, '/member/new?result=error');
+        if (err) res.redirect(302, '/app/member/new?result=error');
 
         try {
           const member = await prisma.member.create({
@@ -28,7 +28,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
           res.status(200).json(member);
         } catch (e) {
           console.log('Prismaエラーコード：' + e.code);
-          res.redirect(303, '/member/new?result=error');
+          res.redirect(302, '/app/member/new?result=error');
         }
       });
       break;
