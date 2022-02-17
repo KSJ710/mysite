d9d5c6dc6e93b1a1e5173eacc299ebb2050e723e
KSJ710 <KSJ710@example.com>
2022-02-17 09:47:52 +0900

    Newクラス完成

--- src/components/pages/member/New.tsx
+++ src/components/pages/member/New.tsx
@@ -5,26 +5,54 @@ interface WithRouterProps {
   router: NextRouter;
 }
 
+type State = {
+  name: string;
+  email: string;
+  password: string;
+};
+
 class New extends React.Component<WithRouterProps> {
+  state: State;
+
   constructor(props) {
     super(props);
     this.state = { name: '', email: '', password: '' };
+    this.changeName = this.changeName.bind(this);
+    this.changeEmail = this.changeEmail.bind(this);
+    this.changePassword = this.changePassword.bind(this);
   }
+
+  changeName(e) {
+    this.setState({ name: e.target.value });
+  }
+  changeEmail(e) {
+    this.setState({ email: e.target.value });
+  }
+  changePassword(e) {
+    this.setState({ password: e.target.value });
+  }
+
   render() {
     return (
       <>
         <form action="/api/member/create" method="post">
           <label htmlFor="name">
             Name:
-            <input type="text" name="name" className="border-2" />
+            <input type="text" value={this.state.name} name="name" className="border-2" onChange={this.changeName} />
           </label>
           <label htmlFor="email">
             email:
-            <input type="text" name="email" className="border-2" />
+            <input type="text" value={this.state.email} className="border-2" onChange={this.changeEmail} />
           </label>
           <label htmlFor="password">
             Password:
-            <input type="password" name="password" className="border-2" />
+            <input
+              type="password"
+              value={this.state.password}
+              name="password"
+              className="border-2"
+              onChange={this.changePassword}
+            />
           </label>
           <input type="submit" value="Submit" className="px-4 mt-2 bg-cyan-300 rounded-full " />
         </form>
