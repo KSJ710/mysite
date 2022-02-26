7edd100df20421f93f8621c226aa1140dc748d1f
KSJ710 <KSJ710@example.com>
2022-02-26 19:58:15 +0900

    ログアウト関数を使用

--- src/components/layout/Menu.tsx
+++ src/components/layout/Menu.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable tailwindcss/no-custom-classname */
 import { useSession } from 'next-auth/react';
+import { signOut } from 'next-auth/react';
 // component
 import List from 'src/components/general/List';
 // css
@@ -11,15 +12,48 @@ const Head = (props): JSX.Element => {
   let list = [];
   if (status === 'authenticated') {
     list = [
-      { value: 'ホーム', href: '/home', shallow: true, event: props.handleTglDisp },
-      { value: 'ログアウト', href: '/api/auth/signout', shallow: false, event: props.handleTglDisp },
-      { value: 'マイページ', href: '/mypage', shallow: true, event: props.handleTglDisp },
+      {
+        value: 'ホーム',
+        href: '/home',
+        shallow: true,
+        event: props.handleTglDisp,
+      },
+      {
+        value: 'ログアウト',
+        href: '/credentials-signout',
+        shallow: false,
+        event: () =>
+          signOut({
+            callbackUrl: 'http://localhost:3000/auth/credentials-signin',
+          }),
+      },
+      {
+        value: 'マイページ',
+        href: '/mypage',
+        shallow: true,
+        event: props.handleTglDisp,
+      },
     ];
   } else {
     list = [
-      { value: 'ホーム', href: '/home', shallow: true, event: props.handleTglDisp },
-      { value: 'ログイン', href: '/auth/credentials-signin', shallow: false, event: props.handleTglDisp },
-      { value: 'アカウント作成', href: '/member/new', shallow: true, event: props.handleTglDisp },
+      {
+        value: 'ホーム',
+        href: '/home',
+        shallow: true,
+        event: props.handleTglDisp,
+      },
+      {
+        value: 'ログイン',
+        href: '/auth/credentials-signin',
+        shallow: false,
+        event: props.handleTglDisp,
+      },
+      {
+        value: 'アカウント作成',
+        href: '/member/new',
+        shallow: true,
+        event: props.handleTglDisp,
+      },
     ];
   }
 
