a9e5e0f9d68529f477a4fc77b56d33a21b082f22
KSJ710 <KSJ710@example.com>
2022-09-29 23:14:01 +0900

    アニメリンク追加

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -10,6 +10,12 @@ export default function Home(): JSX.Element {
           <a className="inline-block py-2 px-4 bg-black rounded-full">テンプレート</a>
         </Link>
       </section>
+      <section className={`${styles.guidAccount} mb-[40px]`}>
+        <h2 className="mb-[20px]">タイトル</h2>
+        <Link href="/sample">
+          <a className="inline-block py-2 px-4 bg-black rounded-full">ロードアニメーション</a>
+        </Link>
+      </section>
     </div>
   );
 }
